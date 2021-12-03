9bf2a6fc41644b16ad842d67ada27d13afe8cf73
Bryan Veneruso <bryanv@qualtrics.com>
2021-12-03 10:27:30 -0800

    Refactored p3

--- day3/p1.js
+++ day3/p1.js
@@ -1,29 +1,43 @@
 const util = require('../helpers/util.js');
 
-// 3:26, would've been faster if not having my run messed up :(
-let run = async function() {
-	let totalCount = 0;
+/**
+ * Generates a number with either the most common value for each bit or the least common
+ * @param numbers Input array, numbers should be binary strings
+ * @param findMostCommonBit True to find most common bits, false to find least common
+ * @returns {string} Most or least common combination of bits
+ */
+let findCommonNumber = function(numbers, findMostCommonBit) {
 	let numberOfOnesInIndex = {};
-	let gamma = '';
-	let epsi = '';
+
+	for(let numb of numbers) {
+		let charIndex = 0;
+		for(let char of numb.split('')) {
+			if(!numberOfOnesInIndex.hasOwnProperty(charIndex))
+				numberOfOnesInIndex[charIndex] = 0;
+			if(char === '1')
+				numberOfOnesInIndex[charIndex]++;
+			charIndex++;
+		}
+	}
+
+	let commonString = Object.values(numberOfOnesInIndex).map(numbOnes => (numbOnes >= numbers.length - numbOnes) ? '1' : '0').join('');
+	return findMostCommonBit ? commonString : inverseBinaryString(commonString);
+}
+
+let inverseBinaryString = function(text) {
+	return [...text].map(char => (char === '1') ? '0' : '1').join('');
+}
+
+let run = async function() {
+	let numbers = [];
 
 	await util.readFile(`${__dirname}/p1input.txt`, function(line) {
-		totalCount++;
-		gamma = '';
-		epsi = '';
-		for(let i = 0; i < line.length; i++) {
-			if(line.charAt(i) === '1') {
-				if(!numberOfOnesInIndex.hasOwnProperty(i))
-					numberOfOnesInIndex[i] = 0;
-				numberOfOnesInIndex[i]++;
-			}
-
-			let oneCount = numberOfOnesInIndex.hasOwnProperty(i) ? numberOfOnesInIndex[i] : 0;
-			gamma += (oneCount > totalCount - oneCount) ? '1' : '0';
-			epsi += (oneCount > totalCount - oneCount) ? '0' : '1';
-		}
+		numbers.push(line);
 	});
-	console.log('gamma is ' + gamma + ' and epsi is ' + epsi);
+
+	let gamma = findCommonNumber(numbers, true);
+	let epsi = findCommonNumber(numbers, false);
+
 	return parseInt(gamma, 2) * parseInt(epsi, 2);
 }
 
--- run.js
+++ run.js
@@ -6,20 +6,20 @@ const day3p1 = require('./day3/p1.js');
 const day3p2 = require('./day3/p2.js');
 
 let run = async function() {
-	console.log('day1 p1');
-	console.log(await day1p1.run());
-
-	console.log('day1 p2');
-	console.log(await day1p2.run());
-
-	console.log('day2 p1');
-	console.log(await day2p1.run());
-
-	console.log('day2 p2');
-	console.log(await day2p2.run());
-
-	console.log('day3 p1');
-	console.log(await day3p1.run());
+	// console.log('day1 p1');
+	// console.log(await day1p1.run());
+	//
+	// console.log('day1 p2');
+	// console.log(await day1p2.run());
+	//
+	// console.log('day2 p1');
+	// console.log(await day2p1.run());
+	//
+	// console.log('day2 p2');
+	// console.log(await day2p2.run());
+	//
+	// console.log('day3 p1');
+	// console.log(await day3p1.run());
 
 	console.log('day3 p2');
 	console.log(await day3p2.run());
